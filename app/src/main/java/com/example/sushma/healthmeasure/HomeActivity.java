c24ed7e378c99d63823aeba87f80ea11622d1b8f
apavate <apavate@miraclesoft07.onmicrosoft.com>
2019-02-11 11:04:52 -0500

    Initial commit

---
+++ app/src/main/java/com/example/sushma/healthmeasure/HomeActivity.java
@@ -0,0 +1,44 @@
+package com.example.sushma.healthmeasure;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    //mTextMessage.setText("Home");
+                    return true;
+                case R.id.navigation_dashboard:
+                    //mTextMessage.setText("Graphs");
+                    return true;
+                case R.id.navigation_notifications:
+                    //mTextMessage.setText("Plans");
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
