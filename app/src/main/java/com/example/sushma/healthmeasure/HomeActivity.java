d65aec4500c01aee0164390293c3d8dec942dd10
apavate <apavate@miraclesoft07.onmicrosoft.com>
2019-03-06 18:00:16 -0500

    Finished About us and other pages

--- app/src/main/java/com/example/sushma/healthmeasure/HomeActivity.java
+++ app/src/main/java/com/example/sushma/healthmeasure/HomeActivity.java
@@ -1,35 +1,78 @@
 package com.example.sushma.healthmeasure;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    Dialog myDialog;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     //mTextMessage.setText("Home");
-                    return true;
+                    selectedFragment = new WaterFragment();
+                    //return true;
+                    break;
                 case R.id.navigation_dashboard:
                     //mTextMessage.setText("Graphs");
-                    return true;
+                    selectedFragment = new GraphFragment();
+                    //return true;
+                    break;
                 case R.id.navigation_notifications:
                     //mTextMessage.setText("Plans");
-                    return true;
+                    selectedFragment = new PlanFragment();
+                    break;
             }
-            return false;
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+            return true;
         }
     };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_actions,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+
+            case R.id.item1:
+                //Log.d("switch","Entered item 1");
+                myDialog.setContentView(R.layout.popup);
+                myDialog.show();
+                //Toast.makeText(this,"Item 1 selected",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item2:
+                //Log.d("switch","Entered item 2");
+                Toast.makeText(this,"Item 2 selected",Toast.LENGTH_SHORT).show();
+                return true;
+            //default:  return super.onOptionsItemSelected(item);
+
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +82,9 @@ public class HomeActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new WaterFragment()).commit();
+        myDialog = new Dialog(this);
     }
 
 }
